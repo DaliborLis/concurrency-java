1e20cbe22ba9bbf1149d8e35c128bedc3e759bbe
Dalibor Lis <dalibor@10.0.0.60>
2026-05-28 11:25:54 +0200

    Improve numbers printing

--- src/main/java/concurrency/java/printsequence/PrintNumberInOrder.java
+++ src/main/java/concurrency/java/printsequence/PrintNumberInOrder.java
@@ -6,21 +6,26 @@ import java.util.stream.IntStream;
 public class PrintNumberInOrder {
 
     private static final int THREAD_COUNT = 3;
+    private static final int ITERATIONS = 10;
 
     private static final Object lock = new Object();
     private static int counter = 0;
 
     public static void printSequence(int threadId) {
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < ITERATIONS; i++) {
             synchronized (lock) {
-                while (counter % THREAD_COUNT != threadId) {
+                while (counter < THREAD_COUNT * ITERATIONS && counter % THREAD_COUNT != threadId) {
                     try {
                         lock.wait();
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                         return;
                     }
+                }
+                if (counter >= THREAD_COUNT * ITERATIONS) {
+                    lock.notifyAll();
+                    return;
                 }
                 System.out.printf("Thread %d - %d%n", threadId, counter);
                 counter++;
